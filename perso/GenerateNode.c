3684920e6bfe22b91cd33d338541f6add2d1fc83
sacha.memmi <sacha.memmi@etu.upmc.fr>
2020-02-09 16:04:25 +0100

    probleme avec highest_frequency

--- perso/GenerateNode.c
+++ perso/GenerateNode.c
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <stdbool.h>
 #include "GenerateNode.h"
-#define p 0.2
-#define q 0.01
+#define p 0.8
+#define q 0.2
 
 adjmatrix *generate_graph(int n_nodes, int n_clusters){
   int e1 = NLINKS;
@@ -12,6 +12,10 @@ adjmatrix *generate_graph(int n_nodes, int n_clusters){
   g->n = n_nodes;
   g->mat = malloc(sizeof(bool)*g->n*g->n);
   unsigned long *array_nodes = malloc(sizeof(unsigned long)*g->n);
+  for(unsigned long i=0; i<g->n; i++)
+    {
+      array_nodes[i] = i;
+    }
   unsigned long *shuffled_array = fisher_yates(array_nodes,g->n);
   g->edges = malloc(e1*sizeof(edge));
 
@@ -19,7 +23,7 @@ adjmatrix *generate_graph(int n_nodes, int n_clusters){
     {
       int cluster_1 = i/(n_nodes/n_clusters);
       
-      for(int j=i; j<n_nodes;j++)
+      for(int j=i+1; j<n_nodes;j++)
 	{
 	  int cluster_2 = j/(n_nodes/n_clusters);
 	  float ra = (float) (rand()%100)/100;
@@ -27,11 +31,12 @@ adjmatrix *generate_graph(int n_nodes, int n_clusters){
 	  
 	  if(cluster_1 == cluster_2)
 	    {
+	      printf("Rentre dans le if du meme cluster\n");
 	      if(ra<p)
 		{//Création de l'arête entre deux noeuds du même cluster
-		  //printf("On rentre dans le if du meme cluster\n");
-		  g->edges[g->e].s = shuffled_array[i] ;
-		  g->edges[g->e].t = shuffled_array[j];
+		  printf("On rentre dans le if du meme cluster random < p\n");
+		  g->edges[g->e].s = i;//shuffled_array[i] ;
+		  g->edges[g->e].t = j;
 		  if (++(g->e)==e1) {//increase allocated RAM if needed
 		    e1+=NLINKS;
 		    g->edges=realloc(g->edges,e1*sizeof(edge));
@@ -41,10 +46,11 @@ adjmatrix *generate_graph(int n_nodes, int n_clusters){
 	    }
 	  else
 	    {
+	      printf("Rentre dans le if du cluster different \n");
 	      if(ra<q)
 		{//liaison avec un noeud d'un cluster avec un cluster different
-		  g->edges[g->e].s = shuffled_array[i];
-		  g->edges[g->e].t = shuffled_array[j];
+		  g->edges[g->e].s = i;
+		  g->edges[g->e].t = j;
 		  if (++(g->e)==e1) {//increase allocated RAM if needed
 		    e1+=NLINKS;
 		    g->edges=realloc(g->edges,e1*sizeof(edge));
@@ -72,7 +78,43 @@ unsigned long *fisher_yates(unsigned long *array, int length)
   return array;
 }
 
-adjmatrix* label_propagation(adjmatrix *g)
+int checkLabelPropagationEnds(adjmatrix *g, unsigned long *labels){
+
+  for(unsigned long i=0; i<g->n; i++)
+    {
+      
+      if(labels[i]!=highest_frequency(g,i,labels))
+	return 1;
+    }
+  return 0;
+}
+
+unsigned long* label_propagation(adjmatrix *g)
+{
+  unsigned long *labels = malloc(g->n*sizeof(unsigned long));
+  for(int i=0; i<g->n; i++)
+    {
+      labels[i] = i;
+    }
+  
+  //Randomisation
+  //adjmatrix *copy = fisher_yates_adj(g);
+  int k = 0;
+  while(k<1)//checkLabelPropagationEnds(g,labels)!=0)
+    {
+      for(int i=0; i<g->n;i++)
+	{
+	  unsigned long nd = highest_frequency(g,i,labels);	  
+	  printf("node label most viewed %lu\n",nd);
+	  printf("plus present voisin de %lu : %lu",i,nd);	  
+	  labels[i] = nd;	 
+	}
+      k++;
+    }
+  return labels;
+}
+
+adjmatrix* fisher_yates_adj(adjmatrix *g)
 {
   for(int i=1;i<=g->n;i++)
     {
@@ -84,13 +126,124 @@ adjmatrix* label_propagation(adjmatrix *g)
  
       }
     }
+  return g;
 }
 
-adjmatrix* fisher_yates_adj(adjmatrix *g)
+unsigned long highest_frequency(adjmatrix *g, unsigned long u, unsigned long *labels)
 {
+  
+  //Extraction du nombre de labels/communautés différents
+  /*unsigned long *copy = malloc(g->n*sizeof(unsigned long)) ;
+  memcpy(copy,labels,g->n*sizeof(unsigned long));
+
+  unsigned long lastSeen = 0;
+  int count_different_labels = 0 ;
+  for(int i=0; i<g->n; i++)
+    {
+      if(copy[i]!=lastSeen)
+	{
+	  lastSeen = copy[i];
+	  count_different_labels++;
+	}
+    }  
+  */
+  printf("Affichage tableau labels\n");
+
+  for(int i=0;i<g->n;i++)
+    {
+      printf("%lu,",labels[i]);      
+    }
+  printf("\n____________\n");
+  
+  unsigned long *array = calloc(g->n,sizeof(unsigned long));
+  for(int i=0;i<g->n;i++)
+    {
+      if(g->mat[u*g->n + i]==1)
+	{
+	  printf("noeud : %lu labels : %lu\n",i,labels[i]);
+	  array[labels[i]]++ ;
+	}
+    }
+   printf("Affichage tableau\n");
+     for(int i=0;i<g->n;i++)
+    {
+      printf("%lu,",array[labels[i]]);
+    }
+  unsigned long max = 0;
+  unsigned long max_node=0;
+  for(int i=0;i<g->n;i++)
+    {
+      
+      if(array[i]>=max && labels[i]!=i)
+	{
+	  //printf("Rentre dans le if de la recherche du max\n");
+	  max = array[i];
+	  max_node = labels[i];	  
+	}
+    }
+  return max_node;
+    
+}
+
+void mergeSort(unsigned long *array, int l, int r)
+{
+  if(l<r)
+    {
+      int m = l + (r-l)/2;
+      mergeSort(array,l,m);
+      mergeSort(array,m+1,r);
+      merge(array,l,m,r);
+    }
   
 }
 
+void merge(unsigned long *array, int debut,int pivot, int fin)
+{
+  int i,j,k;
+  int n1 = pivot - debut + 1;
+  int n2 = fin-pivot;
+  int *L = calloc(n1,sizeof(int));
+  int *R = calloc(n2,sizeof(int));
+  for(i=0;i<n1;i++)
+    {
+      L[i] = array[debut+i];      
+    }
+  for(j=0; j<n2; j++)
+    {
+      R[j] = array[pivot+1+j];
+    }
+  i = 0;
+  j = 0;
+  k = debut;
+  while(i<n1 && j<n2)
+    {
+      if(L[i]<=R[i])
+	{
+	  array[k] = R[j];
+	  i++;
+	}
+      else
+	{
+	  array[k] = L[i];
+	  j++;
+	}
+      k++;
+    }
+  while(i<n1)
+    {
+      array[k] = L[i];
+      k++;
+      i++;
+    }
+  while(j<n2)
+    {
+      array[k] = R[j];
+      k++;
+      j++;
+    }
+  
+  
+}
 int main(int argc, char **argv)
 {
   srand(time(NULL));
@@ -100,26 +253,33 @@ int main(int argc, char **argv)
   t1=time(NULL);
 
   //printf("Reading edgelist from file %s\n",argv[1]);
-  g=generate_graph(400,4);
+  g=generate_graph(6,2);
 
+  
   printf("Number of nodes: %lu\n",g->n);
   printf("Number of edges: %lu\n",g->e);
 
   printf("Building the adjacency matrix\n");
   mkmatrix(g);
+  for(int i =0; i<g->n;i++)
+    {
+      printf("noeud %lu:\n",i);
+      for(int j =0; j<g->n; j++)
+	{
+	  if(g->mat[g->n *i +j]==1)
+	    printf("nd %u,",j);	  
+	}
+      printf("\n");
+    }
   
+  unsigned long *labels = label_propagation(g);
+  /*printf("Affichage tableau\n");
+
   for(int i=0;i<g->n;i++)
     {
-      printf("Noeud %d :\n",i);
-      for(int j=0; j<g->n; j++)
-	{
-	   if(g->mat[i * g->n +j]==1)
-	    {
-	      printf("%d,",j);
-	    }
-	}
-	   printf("\n");
-	}
+      printf("%lu,",labels[i]);      
+    }
+    printf("\n____________\n");*/
   free_adjmatrix(g);
 
   t2=time(NULL);
--- perso/GenerateNode.h
+++ perso/GenerateNode.h
@@ -2,5 +2,12 @@
 
 adjmatrix *generate_graph(int n_nodes, int n_clusters);
 unsigned long *fisher_yates(unsigned long *array, int length);
-adjmatrix* label_propagation(adjmatrix *g);
+unsigned long* label_propagation(adjmatrix *g);
 adjmatrix* fisher_yates_adj(adjmatrix *g);
+unsigned long highest_frequency(adjmatrix *g, unsigned long u, unsigned long *labels);
+
+//Check if each node have the highest frequency among its neighbours
+int checkLabelPropagationEnds(adjmatrix *g, unsigned long *labels);
+
+void mergeSort(unsigned long *array, int l, int r);
+void merge(unsigned long *array, int debut,int pivot, int fin);
